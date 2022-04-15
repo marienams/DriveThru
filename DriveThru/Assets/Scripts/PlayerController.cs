68febfbaffa7011eb830764e67b3110ce6c7adc4
Mahima SIngh <mahima.ms0007@gmail.com>
2022-04-15 13:39:42 +0200

    Local Multiplayer
    added player 2, 2 input manager IDs for 2 player, both with respective camera switch keys

--- DriveThru/Assets/Scripts/PlayerController.cs
+++ DriveThru/Assets/Scripts/PlayerController.cs
@@ -8,10 +8,12 @@ public class PlayerController : MonoBehaviour
     public float speed = 5.0f;
     public float turnSpeed;
     public float horizontalInput;
+    
     public float forwardInput;
     public Camera mainCamera;
     public Camera hoodCamera;
     public KeyCode switchKey;
+    public string inputID; //for local multiplayer
     // Start is called before the first frame update
     void Start()
     {
@@ -22,8 +24,10 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //we will move forward our vehicle
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        //horizontalInput = Input.GetAxis("Horizontal");
+        //forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("horizontal" + inputID);
+        forwardInput = Input.GetAxis("vertical" + inputID);
         transform.Translate(Vector3.forward * Time.deltaTime * speed *forwardInput);
         //transform.Rotate(Vector3.forward * Time.deltaTime * turnSpeed *horizontalInput);
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
@@ -33,5 +37,6 @@ public class PlayerController : MonoBehaviour
             mainCamera.enabled = !mainCamera.enabled;
             hoodCamera.enabled = !hoodCamera.enabled;
         }
+        
     }
 }
