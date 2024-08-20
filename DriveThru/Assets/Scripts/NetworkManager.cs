59c54ce43e4b94b83be54902c8c91a056e5c6740
marienams <mahima.ms0007@gmail.com>
2024-08-20 10:03:37 +0530

    added fusion starter script to allow hosting game or joining game. made the player a network object, player spawning on done at this point

---
+++ DriveThru/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,164 @@
+using System;
+using System.Collections.Generic;
+using Fusion;
+using Fusion.Sockets;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NetworkManager : MonoBehaviour, INetworkRunnerCallbacks
+{
+    private NetworkRunner _runner;
+
+    async void StartGame(GameMode mode)
+    {
+        // Create the Fusion runner and let it know that we will be providing user input
+        _runner = gameObject.AddComponent<NetworkRunner>();
+        _runner.ProvideInput = true;
+
+        // Create the NetworkSceneInfo from the current scene
+        //var scene = SceneRef.FromIndex(SceneManager.GetActiveScene().buildIndex);
+        var scene = SceneRef.FromIndex(1);
+        var sceneInfo = new NetworkSceneInfo();
+        if (scene.IsValid) {
+            sceneInfo.AddSceneRef(scene, LoadSceneMode.Additive);
+        }
+
+        // Start or join (depends on gamemode) a session with a specific name
+        await _runner.StartGame(new StartGameArgs()
+        {
+            GameMode = mode,
+            SessionName = "TestRoom",
+            Scene = scene,
+            SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>()
+        });
+    }
+    public void HostGame(){
+        StartGame(GameMode.Host);
+    }
+    public void JoinGame(){
+        StartGame(GameMode.Client);
+    }
+    public void QuitGame() {
+        Application.Quit();
+    }
+    // private void OnGUI()
+    // {
+    // if (_runner == null)
+    // {
+    //     if (GUI.Button(new Rect(0,0,200,40), "Host"))
+    //     {
+    //         StartGame(GameMode.Host);
+    //     }
+    //     if (GUI.Button(new Rect(0,40,200,40), "Join"))
+    //     {
+    //         StartGame(GameMode.Client);
+    //     }
+    // }
+    // }
+    public void OnConnectedToServer(NetworkRunner runner)
+    {
+        Debug.Log("Event: OnConnectedToServer");
+    }
+
+    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
+    {
+        Debug.Log("Event: OnConnectFailed");
+    }
+
+    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token)
+    {
+        Debug.Log("Event: OnConnectRequest");
+    }
+
+    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data)
+    {
+       Debug.Log("Event: OnCustomAuthenticationResponse");
+    }
+
+    public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason)
+    {
+       Debug.Log("Event: OnDisconnectedFromServer");
+    }
+
+    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken)
+    {
+        Debug.Log("Event: OnHostMigration");
+    }
+
+    public void OnInput(NetworkRunner runner, NetworkInput input)
+    {
+        Debug.Log("Event: OnInput");
+    }
+
+    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input)
+    {
+        Debug.Log("EventOnInputMissing");
+    }
+
+    public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
+    {
+        Debug.Log("Event: OnObjectEnterAOI");
+    }
+
+    public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
+    {
+        Debug.Log("Event: OnObjectExitAOI");
+    }
+
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+    {
+        Debug.Log("Event: OnPlayerJoined");
+    }
+
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
+    {
+       Debug.Log("Event: OnPlayerLeft");
+    }
+
+    public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress)
+    {
+       Debug.Log("Event: OnReliableDataProgress");
+    }
+
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data)
+    {
+       Debug.Log("Event: OnReliableDataReceived");
+    }
+
+    public void OnSceneLoadDone(NetworkRunner runner)
+    {
+        Debug.Log("Event: OnSceneLoadDone");
+    }
+
+    public void OnSceneLoadStart(NetworkRunner runner)
+    {
+        Debug.Log("Event: OnSceneLoadStart");
+    }
+
+    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
+    {
+        Debug.Log("Event: OnSessionListUpdated");
+    }
+
+    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
+    {
+        Debug.Log("Event: OnShutdown");
+    }
+
+    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message)
+    {
+       Debug.Log("Event: OnUserSimulationMessage");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
